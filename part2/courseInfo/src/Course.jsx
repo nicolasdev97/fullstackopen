61826d7aa6c3f6ce8181a5158eb957e1f54baf19
nicolasdev97 <nicolas.po.im@gmail.com>
2026-01-06 16:30:15 -0500

    Exercise 2.3.

--- part2/courseInfo/src/Course.jsx
+++ part2/courseInfo/src/Course.jsx
@@ -3,11 +3,15 @@ import Header from "./Header";
 import Total from "./Total";
 
 const Course = ({ course }) => {
+  const totalExercises = course.parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  );
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Total total={totalExercises} />
     </div>
   );
 };
--- part2/courseInfo/src/Total.jsx
+++ part2/courseInfo/src/Total.jsx
@@ -1,7 +1,2 @@
-const Total = ({ parts }) => (
-  <p>
-    Number of exercises{" "}
-    {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-  </p>
-);
+const Total = ({ total }) => <p>Number of exercises {total}</p>;
 export default Total;
